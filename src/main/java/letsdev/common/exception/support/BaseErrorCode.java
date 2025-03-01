f3062809528b09f977213319e1785b7e77f61668
merge-simpson <letsdev@kakao.com>
2025-03-02 02:08:39 +0900

    chore(base): fix documentation comment of `BaseErrorCode` a little

--- src/main/java/letsdev/common/exception/support/BaseErrorCode.java
+++ src/main/java/letsdev/common/exception/support/BaseErrorCode.java
@@ -1,7 +1,7 @@
 package letsdev.common.exception.support;
 
 /**
- * Independent on Spring Framework or etc.
+ * This interface is independent of any frameworks such as Spring.
  */
 public interface BaseErrorCode {
     String name(); // automatically overridden in enum
