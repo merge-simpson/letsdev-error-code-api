2b26b0b3917b29cd6888920a4647ed0ada18d3bb
merge-simpson <letsdev@kakao.com>
2025-03-02 02:55:00 +0900

    feat(spring dependent): make `CustomException` extend `BaseCustomException`

--- src/main/java/letsdev/common/exception/support/CustomException.java
+++ src/main/java/letsdev/common/exception/support/CustomException.java
@@ -1,136 +1,45 @@
 package letsdev.common.exception.support;
 
-import org.springframework.http.HttpStatus;
-
-import java.util.Collections;
 import java.util.Map;
 import java.util.function.Supplier;
 
-public class CustomException extends RuntimeException {
-
-    protected final ErrorCode errorCode;
-    protected final Runnable action;
-    protected final Supplier<Map<String, Object>> payloadSupplier;
+public class CustomException extends BaseCustomException {
 
     public CustomException() {
-        super(getDefaultErrorCode().message());
-        this.errorCode = getDefaultErrorCode();
-        this.action = () -> {};
-        this.payloadSupplier = Collections::emptyMap;
+        super();
     }
 
     public CustomException(String message) {
         super(message);
-        this.errorCode = getDefaultErrorCode();
-        this.action = () -> {};
-        this.payloadSupplier = Collections::emptyMap;
     }
 
     public CustomException(String message, Throwable cause) {
         super(message, cause);
-        this.errorCode = getDefaultErrorCode();
-        this.action = () -> {};
-        this.payloadSupplier = Collections::emptyMap;
     }
 
     public CustomException(ErrorCode errorCode) {
-        super(errorCode.message());
-        this.errorCode = errorCode;
-        this.action = () -> {};
-        this.payloadSupplier = Collections::emptyMap;
+        super(errorCode);
     }
 
     public CustomException(ErrorCode errorCode, Throwable cause) {
-        super(errorCode.message(), cause);
-        this.errorCode = errorCode;
-        this.action = () -> {};
-        this.payloadSupplier = Collections::emptyMap;
+        super(errorCode, cause);
     }
 
     public CustomException(ErrorCode errorCode, Runnable action) {
-        super(errorCode.message());
-        this.errorCode = errorCode;
-        this.action = action;
-        this.payloadSupplier = Collections::emptyMap;
+        super(errorCode, action);
     }
 
     public CustomException(ErrorCode errorCode, Runnable action, Throwable cause) {
-        super(errorCode.message(), cause);
-        this.errorCode = errorCode;
-        this.action = action;
-        this.payloadSupplier = Collections::emptyMap;
+        super(errorCode, action, cause);
 
     }
 
     public CustomException(ErrorCode errorCode, Supplier<Map<String, Object>> payloadSupplier) {
-        super(errorCode.message());
-        this.errorCode = errorCode;
-        this.action = () -> {};
-        this.payloadSupplier = payloadSupplier;
+        super(errorCode, payloadSupplier);
 
     }
 
     public CustomException(ErrorCode errorCode, Supplier<Map<String, Object>> payloadSupplier, Throwable cause) {
-        super(errorCode.message(), cause);
-        this.errorCode = errorCode;
-        this.action = () -> {};
-        this.payloadSupplier = payloadSupplier;
-    }
-
-    private static ErrorCode getDefaultErrorCode() {
-        return DefaultErrorCodeHolder.DEFAULT_ERROR_CODE;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
-
-    private static class DefaultErrorCodeHolder { // 사용할 때 로드 + 스레드 세이프(클래스 로드 타임은 동시성 보장됨.)
-        private static final ErrorCode DEFAULT_ERROR_CODE = new ErrorCode() {
-            @Override
-            public String name() {
-                return "SERVER_ERROR";
-            }
-
-            @Override
-            public HttpStatus httpStatus() {
-                return HttpStatus.INTERNAL_SERVER_ERROR;
-            }
-
-            @Override
-            public String message() {
-                return "서버 오류";
-            }
-
-            @Override
-            public CustomException exception() {
-                return new CustomException(this);
-            }
-
-            @Override
-            public CustomException exception(Throwable cause) {
-                return new CustomException(this, cause);
-            }
-
-            @Override
-            public CustomException exception(Runnable action) {
-                return new CustomException(this, action);
-            }
-
-            @Override
-            public CustomException exception(Runnable action, Throwable cause) {
-                return new CustomException(this, action, cause);
-            }
-
-            @Override
-            public CustomException exception(Supplier<Map<String, Object>> payloadSupplier) {
-                return new CustomException(this, payloadSupplier);
-            }
-
-            @Override
-            public CustomException exception(Supplier<Map<String, Object>> payloadSupplier, Throwable cause) {
-                return new CustomException(this, payloadSupplier, cause);
-            }
-        };
+        super(errorCode, payloadSupplier, cause);
     }
 }
